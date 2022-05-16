11640500c97eebf186f9f38f811184c8d6b3f9c1
ArtjomLemonezoArt <mrsintesthebest@gmail.com>
2022-05-16 20:34:52 +0300

    Initial commit

---
+++ Task1.py
@@ -0,0 +1,5 @@
+####################Task1
+user_name = input('Enter your name:')
+user_surname = input('Enter your surname')
+user_age = input('Enter your age')
+print(f'Hello, {user_surname} {user_name}. Your age is:{user_age}')
